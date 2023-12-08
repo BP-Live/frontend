e79b42b768a37235e468f0f9520646e15f8f024f
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-12-08 12:12:56 +0100

    Add api calls

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function AppPage() {
   return <div>App Page - This shold be OAuth protected.</div>;
 }
---
+++ lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+export default axiosInstance;
