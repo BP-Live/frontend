a42d2e52cfd57a18e46bcad8ab34209ea9e55c35
Daniel Bacsur <contact@danielbacsur.com>
2023-12-08 22:16:52 +0100

    idk

--- components/details-segment.tsx
+++ components/details-segment.tsx
@@ -3,17 +3,7 @@
 import { Progress } from "@/components/ui/progress";
 import { RestaurantJson } from "@/lib/types";
 
-export function DetailsSegment({
-  status,
-  businessType,
-  businessLocation,
-  json,
-}: {
-  status: number;
-  businessType: string;
-  businessLocation: string;
-  json: RestaurantJson | null;
-}) {
+export function DetailsSegment({ json }: { json: RestaurantJson | null }) {
   return (
     <div className="flex flex-col w-full justify-center items-center">
       <h1 className="text-4xl font-bold my-3 text-center text-white">
@@ -30,13 +20,17 @@ export function DetailsSegment({
           <p className="text-xl font-bold text-center text-white">
             Business type:
           </p>
-          <p className="text-lg text-center text-white">{businessType}</p>
+          <p className="text-lg text-center text-white">
+            {json?.metadata?.type}
+          </p>
         </div>
         <div className="flex flex-row w-full justify-center items-center gap-3 border-2 border-white rounded-xl p-2">
           <h2 className="text-xl font-bold text-center text-white">
             Business location:
           </h2>
-          <h3 className="text-lg text-center text-white">{businessLocation}</h3>
+          <h3 className="text-lg text-center text-white">
+            {json?.metadata?.location}
+          </h3>
         </div>
       </div>
     </div>
