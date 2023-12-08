664a6467175acbe487eb00b4856df9fc1953ac2c
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-12-08 15:29:56 +0100

    Add fonts and google icon

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import Image from "next/image";
 import BKLiveImage from "@/assets/images/bpLive.png";
+import GoogleIcon from "@/assets/images/googleIcon.webp";
 
 export default function HomePage() {
   const { toast } = useToast();
@@ -20,11 +21,22 @@ export default function HomePage() {
   };
 
   return (
-    <div className="flex flex-col w-full justify-center items-center min-h-[100vh]">
-      <div className="flex w-full items-center justify-around">
+    <div className="flex flex-col w-full justify-center items-center">
+      <div className="flex w-full min-h-[100vh] items-center justify-center gap-16 flex-col lg:justify-around lg:flex-row lg:gap-0">
         <Image src={BKLiveImage} width={300} height={300} alt="Budapest Live" />
 
-        <Button onClick={handleGoogleLogin}>Login with Google</Button>
+        <div className="flex items-center justify-center flex-col">
+          <h1 className="text-5xl font-bold font-caption mb-3 text-center">
+            BP Live Project
+          </h1>
+          <h2 className="text-2xl font-caption mb-8 text-center">
+            Making people&apos;s life easier
+          </h2>
+          <Button onClick={handleGoogleLogin} size="lg">
+            <p className="font-bold mr-1">Login with Google</p>
+            <Image src={GoogleIcon} width={35} height={35} alt="Google Icon" />
+          </Button>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,6 +70,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        DEFAULT: ["var(--font-inter)"],
+        caption: ["var(--font-caption)"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
