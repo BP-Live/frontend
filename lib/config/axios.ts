3a24cf4be316ad38668753786eec3addbbe799d2
Daniel Bacsur <contact@danielbacsur.com>
2023-12-08 13:55:49 +0100

    Add check for NEXT_PUBLIC_BACKEND_URL env key

--- lib/config/axios.ts
+++ lib/config/axios.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+if (!process.env.NEXT_PUBLIC_BACKEND_URL) {
+  throw new Error("NEXT_PUBLIC_BACKEND_URL not found.");
+}
+
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
