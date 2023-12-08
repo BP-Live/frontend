48f326655f2334918d6dd331557d722a61a6f4c3
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-12-08 14:47:03 +0100

    Add new google api

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -1,17 +1,5 @@
 import axios from "@/lib/config/axios";
 
-export async function googleAuthAPI() {
-  try {
-    const response = await axios.get("/v1/accounts/google");
-
-    const data = response.data;
-
-    return data;
-  } catch (error) {
-    return error;
-  }
-}
-
 export async function logoutAPI() {
   try {
     const response = await axios.get("/v1/accounts/logout");
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { googleAuthAPI } from "../api";
 import { useToast } from "@/components/ui/use-toast";
+import Image from "next/image";
+import BKLiveImage from "@/assets/images/bpLive.png";
 
 export default function HomePage() {
   const { toast } = useToast();
 
   const handleGoogleLogin = async () => {
     try {
-      await googleAuthAPI();
+      window.location.href = `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/accounts/google`;
     } catch (error) {
       toast({
         title: "Error",
@@ -19,10 +20,12 @@ export default function HomePage() {
   };
 
   return (
-    <div className="flex flex-col w-full justify-center items-center">
-      <h1>Home Page - Hello World</h1>
+    <div className="flex flex-col w-full justify-center items-center min-h-[100vh]">
+      <div className="flex w-full items-center justify-around">
+        <Image src={BKLiveImage} width={300} height={300} alt="Budapest Live" />
 
-      <Button onClick={handleGoogleLogin}>Login with Google</Button>
+        <Button onClick={handleGoogleLogin}>Login with Google</Button>
+      </div>
     </div>
   );
 }
