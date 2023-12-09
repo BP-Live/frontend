92f9a5dc7ee8169b7fb57c7aec2c3d36b2c379ce
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-12-09 03:54:05 +0100

    Check geolocation status

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -60,39 +60,37 @@ export default function AppPage() {
   const [locationLoading, setLocationLoading] = useState(false);
   const [locationDialog, setLocationDialog] = useState(false);
   const [promptLoading, setPromptLoading] = useState(false);
+  const [locationErrorMessage, setLocationErrorMessage] = useState("");
   const [promptDialog, setPromptDialog] = useState(false);
 
   const [json, setJson] = useState<RestaurantJson | null>(null);
-
-  useEffect(() => {
-    console.log(json);
-  }, [json]);
-
-  // TODO: Disable Location Dialog if the user has already given permission
 
   useEffect(() => {
     setLocationDialog(true);
 
     setLocationLoading(true);
 
-    if (!("geolocation" in navigator)) {
-      // Your browser does not support the HTML5 Geolocation API, so this demo will not work.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation([position.coords.longitude, position.coords.latitude]);
+
+          setLocationLoading(false);
+          setLocationDialog(false);
+          setPromptDialog(true);
+        },
+        (error) => {
+          setLocationErrorMessage(error.message);
+          setLocationLoading(false);
+          setLocationDialog(true);
+          setPromptDialog(false);
+        },
+      );
+    } else {
+      setLocationErrorMessage("Geolocation is not supported by this browser.");
+      setPromptDialog(false);
+      setLocationDialog(true);
     }
-
-    navigator.geolocation.getCurrentPosition(
-      async (position) => {
-        setLocation([position.coords.longitude, position.coords.latitude]);
-
-        setLocationLoading(false);
-        setLocationDialog(false);
-        setPromptDialog(true);
-      },
-      () => {
-        setLocationLoading(false);
-        setLocationDialog(false);
-        setPromptDialog(true);
-      },
-    );
   }, []);
 
   useEffect(() => {
@@ -106,22 +104,24 @@ export default function AppPage() {
 
     setLocationLoading(true);
 
-    if (!("geolocation" in navigator)) {
-      // Your browser does not support the HTML5 Geolocation API, so this demo will not work.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          setLocation([position.coords.longitude, position.coords.latitude]);
+
+          setLocationLoading(false);
+          setPromptDialog(true);
+        },
+        () => {
+          setLocationLoading(false);
+          setPromptDialog(true);
+        },
+      );
+    } else {
+      setLocationErrorMessage("Geolocation is not supported by this browser.");
+      setPromptDialog(false);
+      setLocationDialog(true);
     }
-
-    navigator.geolocation.getCurrentPosition(
-      async (position) => {
-        setLocation([position.coords.longitude, position.coords.latitude]);
-
-        setLocationLoading(true);
-        setPromptDialog(true);
-      },
-      () => {
-        setLocationLoading(true);
-        setPromptDialog(true);
-      },
-    );
   }, [locationDialog, requestLocationDialog]);
 
   function onPrompt(values: z.infer<typeof promptSchema>) {
@@ -279,14 +279,18 @@ export default function AppPage() {
       <Dialog open={locationDialog} onOpenChange={setLocationDialog}>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>We Need Your Permission</DialogTitle>
-            <DialogDescription>
-              In order to use this app, you need to enable location.
-            </DialogDescription>
+            <DialogTitle>
+              {locationErrorMessage ? "Error found" : "We Need Your Permission"}
+            </DialogTitle>
+            <DialogDescription>{locationErrorMessage}</DialogDescription>
           </DialogHeader>
           <DialogFooter>
             <Button onClick={() => setLocationDialog(false)} className="w-full">
-              {locationLoading ? "Loading..." : "Enable Location"}
+              {locationLoading
+                ? "Loading..."
+                : locationErrorMessage
+                  ? "Use Default Location"
+                  : "Enable Location"}
             </Button>
           </DialogFooter>
         </DialogContent>
@@ -294,87 +298,3 @@ export default function AppPage() {
     </>
   );
 }
-
-// return (
-// <div
-//   className={cn("h-full", {
-//     "overflow-hidden": !submitted,
-//     "overflow-auto": submitted,
-//   })}
-// >
-//   <div
-//     className={cn(
-//       "absolute left-0 right-0 grid place-items-center transition-all duration-1000 z-20 bg-gradient-to-tl from-bkkPurple to-bkkPink text-foreground",
-//       { "top-0 bottom-0": !submitted, "-top-full bottom-full": submitted }
-//     )}
-//   >
-//     {/* <FormSegment setSubmitted={setSubmitted} setJson={setJson} /> */}
-
-// <Dialog open={open} onOpenChange={setOpen}>
-//   <DialogTrigger asChild>
-//     <Button variant="outline">Edit Profile</Button>
-//   </DialogTrigger>
-//   <DialogContent className="sm:max-w-[425px]">
-//     <DialogHeader>
-//       <DialogTitle>Tell Us Your Idea</DialogTitle>
-//       <DialogDescription>
-//         Describe your idea for a company, so we can tell you some
-//         statistics about it.
-//       </DialogDescription>
-//     </DialogHeader>
-
-//     <Form {...form}>
-//       <form
-//         onSubmit={form.handleSubmit(onSubmit)}
-//         className="space-y-8"
-//       >
-//         <FormField
-//           control={form.control}
-//           name="prompt"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Your Idea</FormLabel>
-//               <FormControl>
-//                 <Textarea
-//                   className="min-h-[20vh]"
-//                   placeholder="My Company is going to be ..."
-//                   {...field}
-//                 />
-//               </FormControl>
-//               <FormDescription>
-//                 Tell us about the type of company you want to start.
-//                 Where do you want to start it? What do you want to sell?
-//               </FormDescription>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-
-//         <DialogFooter>
-//           <Button type="submit" className="w-full">
-//             Save changes
-//           </Button>
-//         </DialogFooter>
-//       </form>
-//     </Form>
-//   </DialogContent>
-// </Dialog>
-//   </div>
-
-//   <div className="fixed top-0 left-0 right-0 lg:right-1/2 bottom-[25vh] lg:bottom-0 grid place-items-center">
-//     <MapSegment />
-//   </div>
-
-//   {submitted ? (
-//     <div className="absolute top-0 left-0 lg:left-1/2 right-0 bottom-0 z-10 pointer-events-none">
-//       <div className="h-[75vh] lg:h-0" />
-//       <div className="min-h-[25vh] lg:text-left lg:min-h-full p-3 bg-gradient-to-tl from-bkkPurple to-bkkPink">
-//         <Header submitted={submitted} />
-//         <DetailsSegment json={json} />
-//       </div>
-//     </div>
-//   ) : (
-//     <Header submitted={submitted} />
-//   )}
-// </div>
-// );
