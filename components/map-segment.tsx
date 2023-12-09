82c7c303d0b7c54b136414fce31897c061395334
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-12-09 03:20:14 +0100

    Cleanup Gergely Daniel's mess

--- components/map-segment.tsx
+++ components/map-segment.tsx
@@ -5,33 +5,24 @@ import { Wrapper } from "@googlemaps/react-wrapper";
 import { useRef, useEffect, useState } from "react";
 // @ts-ignore
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { CSS3DRenderer } from 'three/addons/renderers/CSS3DRenderer.js';
 
 // @ts-ignore
 import { ThreeJSOverlayView } from "@googlemaps/three";
-import { ca } from "date-fns/locale";
 import axios from "@/lib/config/axios";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 import { setInterval } from "timers";
 
 export function MapSegment() {
   return (
-    <Wrapper apiKey={process.env.NEXT_PUBLIC_MAP_API_KEY!} >
+    <Wrapper apiKey={process.env.NEXT_PUBLIC_MAP_API_KEY!}>
       <MapElement />
     </Wrapper>
   );
 }
 
-// meters per degree
-const mDegLat = 111000;
-
 function MapElement() {
   const mapRef = useRef<HTMLDivElement | null>(null);
 
   const [map, setMap] = useState<google.maps.Map | null>(null);
-
-  let data = [];
 
   useEffect(() => {
     if (!mapRef.current) return;
@@ -40,13 +31,11 @@ function MapElement() {
   }, []);
 
   useEffect(() => {
-    const fetchStuff = async () => {
+    const asyncCall = async () => {
       let busReq = await axios.get("/v1/bkk");
-      data = busReq.data;
+
       return busReq.data;
-    }
-
-
+    };
 
     if (!map) return;
 
@@ -63,7 +52,6 @@ function MapElement() {
 
     // Load the model.
     const loader = new GLTFLoader();
-    const fontLoader = new FontLoader();
     const url =
       "https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf";
 
@@ -74,59 +62,37 @@ function MapElement() {
       gltf.scene.position.set(0, 0, 100);
       scene.add(gltf.scene);
 
-      let { tilt, heading, zoom } = cameraOptions;
+      let { heading } = cameraOptions;
 
       const animate = () => {
         heading += 0.001;
         map.notify("render");
-
-        // print what runs when i call the moveCamera function
-
-
-        //gltf.scene.rotation.x += 0.1;
-
-        // render the scene
         requestAnimationFrame(animate);
-      }
+      };
 
       requestAnimationFrame(animate);
     });
 
-    let markers: any[] = [];
+    let markers: google.maps.Marker[] = [];
 
     setInterval(() => {
-      let busReq = fetchStuff().then((data) => {
-        // remove the marker
-        markers.forEach((marker: any) => {
+      asyncCall().then((data) => {
+        markers.forEach((marker: google.maps.Marker) => {
           marker.setMap(null);
         });
 
-
-        console.log(data, "data");
-        data.forEach((bus: any, index: number) => {
-
-
-
-          markers.push(new google.maps.Marker({
-            position: { lat: bus.latitude, lng: bus.longitude },
-            label: bus.vehicle_label,
-            icon: "/bus3.png",
-            map
-          }));
+        data.forEach((bus: any) => {
+          markers.push(
+            new google.maps.Marker({
+              position: { lat: bus.latitude, lng: bus.longitude },
+              label: bus.vehicle_label,
+              icon: "/bus3.png",
+              map,
+            }),
+          );
         });
       });
     }, 5000);
-
-    let colorarray = [
-      0x0000ff,
-      0xff0000,
-      0x00ff00,
-      0xffff00,
-      0x00ffff,
-      0xff00ff,
-    ];
-
-    let index = 0;
 
     //requestAnimationFrame(animate);
 
@@ -163,4 +129,3 @@ async function getMapAsync(ref: HTMLElement): Promise<google.maps.Map> {
   const map = new Map(ref, { ...cameraOptions, ...mapOptions });
   return map;
 }
-
