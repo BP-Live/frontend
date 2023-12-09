ef67197dcc18395ec514f120a55e356e29227a9c
Daniel Bacsur <contact@danielbacsur.com>
2023-12-09 04:09:50 +0100

    Add theme button

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -43,9 +43,19 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { Textarea } from "@/components/ui/textarea";
 import { set } from "date-fns";
 import { Progress } from "@/components/ui/progress";
+import { ToggleThemeButton } from "@/components/toggle-theme-button";
+import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 const promptSchema = z.object({
   prompt: z.string().min(2, {
@@ -156,10 +166,35 @@ export default function AppPage() {
     },
   });
 
+  const { setTheme } = useTheme();
+
   return (
     <>
       <div className="fixed top-0 left-0 bottom-0 right-1/2">
         <MapSegment />
+      </div>
+
+      <div className="absolute top-6 left-6 z-10">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon">
+              <span className="sr-only">Toggle theme</span>
+              <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
       <div className="absolute top-0 left-1/2 bottom-0 right-0 p-6 flex flex-col justify-center">
@@ -184,10 +219,12 @@ export default function AppPage() {
                   <TableCell>Name</TableCell>
                   <TableCell>{json.metadata?.name}</TableCell>
                 </TableRow>
-                {/* <TableRow>
+                <TableRow>
                   <TableCell>Location</TableCell>
-                  <TableCell>{json.metadata?.location}</TableCell>
-                </TableRow> */}
+                  <TableCell>
+                    {JSON.stringify(json.metadata?.location || {})}
+                  </TableCell>
+                </TableRow>
               </TableBody>
             </Table>
 
